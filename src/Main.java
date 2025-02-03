2becce338c4dcea46e8fa175349dd94bf439e526
Mykhailo Savchuk <topms1000@gmail.com>
2025-02-03 14:52:50 +0200

    Is is my second commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         List<String> items = Arrays.asList("Java", "Python", "C++", "JavaScript");
+        System.out.println("haha");
         List<Character> charts = new ArrayList<>();
         for (String item : items) {
             for (char ch : item.toCharArray()) {
